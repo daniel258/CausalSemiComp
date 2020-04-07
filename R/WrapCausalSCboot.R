90adcb4f5d66fc2ad0838faab6c1c5f9abfeee05
Daniel Nevo <danielnevo@gmail.com>
2020-04-07 06:21:35 +0300

    fix bootstrap wrappet output

--- R/WrapCausalSCboot.R
+++ R/WrapCausalSCboot.R
@@ -6,10 +6,9 @@ WrapCausalSCboot <- function(data, i = i, all.times, Ltime, out.times)
                   delta1 = data$delta1[i], delta2 = data$delta2[i], A = data$A[i],
                   all.times = all.times)
   ind.times <- all.times[all.times %in% out.times]
-  res.out <- c(res$etaA0, res$etaA1, res$S2A0.all.times[ind.times], res$S2A0.all.times[ind.times],
-               res$S2A0.all.times[ind.times], res$S2A1.all.times[ind.times],
-               res$etasA0T2.le.t[ind.times], res$etasA1T2.le.t[ind.times],
+  res.out <- c(res$etaA0, res$etaA1, res$S2A0.all.times[ind.times], res$S2A1.all.times[ind.times],
                res$S1A0.all.times[ind.times], res$S1A1.all.times[ind.times],
+               res$etasA0T2.le.t[ind.times], res$etasA1T2.le.t[ind.times],
                res$S1A0T1lT2.all.times[ind.times], res$S1A1T1lT2.all.times[ind.times])
   }
   return(res.out)
