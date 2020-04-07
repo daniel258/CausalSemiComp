0365fa86a68048ef3476907079c33be568b8f909
Daniel Nevo <danielnevo@gmail.com>
2020-04-07 19:23:57 +0300

    Fixed bug in ind.times

--- R/WrapCausalSCboot.R
+++ R/WrapCausalSCboot.R
@@ -5,7 +5,7 @@ WrapCausalSCboot <- function(data, i = i, all.times, Ltime, out.times)
   res <- CausalSC(T1 = data$T1[i], T2 = data$T2[i],
                   delta1 = data$delta1[i], delta2 = data$delta2[i], A = data$A[i],
                   all.times = all.times)
-  ind.times <- all.times[all.times %in% out.times]
+  ind.times <- all.times %in% out.times
   res.out <- c(res$etaA0, res$etaA1, res$S2A0.all.times[ind.times], res$S2A1.all.times[ind.times],
                res$S1A0.all.times[ind.times], res$S1A1.all.times[ind.times],
                res$etasA0T2.le.t[ind.times], res$etasA1T2.le.t[ind.times],
