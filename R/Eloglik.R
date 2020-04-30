4f195f500ed15e73513340658fccbef26dc3b56e
Daniel Nevo <danielnevo@gmail.com>
2020-04-30 08:39:26 +0300

    Update Eloglik

---
+++ R/Eloglik.R
@@ -0,0 +1,12 @@
+Eloglik <- function(theta, delta1, delta2, E.gamma, E.log.gamma)
+{
+  out <- (1/theta)*(log(1/theta))  + (1/theta - 1) * mean(E.log.gamma) -
+    (1 / theta) * mean(E.gamma) - log(gamma(1/theta))
+  #cat("theta = ", theta, ",   g(theta) = ", out, "\n")
+  return(out)
+}
+
+# g_fun <- function(the,mean.Eg,mean.Elg) {
+#   temp <- 1/the*log(1/the) + (1/the - 1)*mean.Elg -1/the*mean.Eg - log(gamma(1/the))
+#   return(-temp)
+# }
