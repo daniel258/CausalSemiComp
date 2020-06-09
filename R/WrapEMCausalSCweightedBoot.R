dd7b7d87a70ba67389120e3ba339d889bbcd1eb4
daniel258 <your_email@example.com>
2020-06-09 08:51:46 +0300

    v 0.1.015: devug boot cluster

--- R/WrapEMCausalSCweightedBoot.R
+++ R/WrapEMCausalSCweightedBoot.R
@@ -9,13 +9,13 @@ while (cond==T) { # cond make sure the bootstrap does not fail
   w <- rexp(n.sample)
   res <- tryCatch(EMcausalSC(data = data, Xnames = Xnames, max.iter = max.iter,
                     w = w, init.thetas = init.thetas), error = function(e) {e})
-  if(inherits(res, "error")){
-    if (try < 10) {try <- try + 1} else {
-      res.out <- c(rep(NA, length.out), try)
-      cond <- F
-    }} else {
+  # if(inherits(res, "error")){
+  #   if (try < 3) {try <- try + 1} else {
+  #     res.out <- c(rep(NA, length.out), try)
+  #     cond <- F
+  #   }} else {
   if (any(res$thetas > 20) | any(res$thetas < 0.02))  {
-    if (try < 10) {try <- try + 1} else {
+    if (try < 3) {try <- try + 1} else {
       res.out <- c(rep(NA, length.out), try)
       cond <- F
     }} else {
@@ -41,7 +41,7 @@ while (cond==T) { # cond make sure the bootstrap does not fail
   res.out <- c(res$naive.betas, res$betas, res$thetas,
                H.A001, H.A002, H.A012, H.A101, H.A102, H.A112, try)
   cond <- F
-    }}}
+    }}
   cat(res.out, "\n")
   return(res.out)
 }
